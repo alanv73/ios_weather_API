22865a38fe58d523702d195ca26ebd1ed110ce51
Alan Van Art <alanv73@gmail.com>
2020-02-12 13:50:02 -0500

    initial commit

--- weatherAPI/ViewController.swift
+++ weatherAPI/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let api_key = "1a803c6d19763d2067ca9953d3b3526c"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       // http://api.openweathermap.org/data/2.5/forecast?q=state%20college&APPID=1a803c6d19763d2067ca9953d3b3526c
+        
+        
     }
 
 

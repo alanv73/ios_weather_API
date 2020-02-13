e940a601b152c3ed9711ca61fb0cb63e826c59b9
Alan Van Art <avanart58@southhills.edu>
2020-02-13 18:31:18 -0500

    add current conditions icon

--- weatherAPI/ViewController.swift
+++ weatherAPI/ViewController.swift
@@ -14,18 +14,20 @@ class ViewController: UIViewController {
     
     @IBOutlet var txtCity: UITextField!
     @IBOutlet var txtResult: UILabel!
+    @IBOutlet weak var imgWx: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
        // https://api.openweathermap.org/data/2.5/forecast?q=state%20college&APPID=1a803c6d19763d2067ca9953d3b3526c
         
-    
+        txtResult.text = ""
         
     }
 
     @IBAction func submit(_ sender: AnyObject) {
         
-        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + txtCity.text!.replacingOccurrences(of: " ", with: "%20") + "&appid=" + api_key) {
+        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + txtCity.text!.replacingOccurrences(of: " ", with: "%20") + "&units=imperial&appid=" + api_key) {
             
             let task = URLSession.shared.dataTask(with: url) {
                 (data, response, error) in
@@ -39,13 +41,21 @@ class ViewController: UIViewController {
                             
                             print(jsonResult)
                             
-                            print(jsonResult["name"]!!)
+                            if(jsonResult["message"] as? String != "city not found") {
+                                print(jsonResult["name"]!!)
+                            }
                             
                             if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
                                 DispatchQueue.main.sync(execute: {
                                     self.txtResult.text = description
                                 })
                             }
+                            if let icon = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["icon"] as? String {
+                                DispatchQueue.main.sync(execute: {
+                                    self.getIcon(icon)
+                                })
+                            }
+                            
                         } catch {
                             print("JSON Processing Failed")
                         }
@@ -58,6 +68,22 @@ class ViewController: UIViewController {
         }
         
         
+    }
+    
+    func getIcon (_ icon: String) {
+        print("http://openweathermap.org/img/wn/\(icon)@2x.png")
+        
+        let url = URL(string:"http://openweathermap.org/img/wn/\(icon)@2x.png")
+
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data, error == nil else { return }
+
+            DispatchQueue.main.async() {    // execute on main thread
+                self.imgWx.image = UIImage(data: data)
+            }
+        }
+
+        task.resume()
     }
 
 }
